222f6fac4a37a3bbaa0a770100a86d2b43388489
James Wilson <jgwilson1214@gmail.com>
2017-12-04 02:17:23 -0800

    begin program

---
+++ slash-star.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("/* SLASH-STAR */\n");
+
+  //read from stdin
+  //look for double slash quotes
+  //replace with slash star quotes
+  //  keep count and display how many changed when done
+
+  return 0;
+}
