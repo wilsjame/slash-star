3017f3a211336c657b84398e901948a358d41d21
James Wilson <jgwilson1214@gmail.com>
2017-12-09 01:26:39 -0800

    add start up usage message

--- slash-star.c
+++ slash-star.c
@@ -1,13 +1,25 @@
-#include <stdio.h>
+//#include <stdio.h>
+#include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-  printf("/* SLASH-STAR */\n");
+
+  /* Print usage */
+  write(0, "Usage ./slashstar -[switch] -[filename]\n", 41);
+  write(0, "Try   ./slashstar -h\n", 22);
+
+  
 
   //read from stdin
+  // check switch
   //look for double slash quotes
   //replace with slash star quotes
   //  keep count and display how many changed when done
 
+
+
+ 
+
   return 0;
 }
+
